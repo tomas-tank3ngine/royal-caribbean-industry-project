862f40c45f3dc062e95f35483ccb8d90640c4939
moescotchman@gmail.com <moescotchman@gmail.com>
2024-01-26 09:01:29 -0800

    Add routes path

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import QuizModal from "./components/QuizModal/QuizModal";
+import PackageSelections from "./pages/PackageSelections/PackageSelections";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/survey" element={<QuizModal />} />
+          <Route path="/packages" element={<PackageSelections />} />
         </Routes>
       </BrowserRouter>
     </>
