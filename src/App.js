b8dceda3f3ffa1c9020cb9f56b60e52b90d5027b
moescotchman@gmail.com <moescotchman@gmail.com>
2024-01-26 08:49:45 -0800

    Fixed styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import "./App.css";
+import "./App.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
+import PackageSelections from "./pages/PackageSelections/PackageSelections";
 
 function App() {
   return (
@@ -8,9 +9,9 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/packages" element={<PackageSelections />} />
         </Routes>
       </BrowserRouter>
-      {/* <button>Click me</button> */}
     </>
   );
 }
