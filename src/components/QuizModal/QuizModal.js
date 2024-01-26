68273459e2ede8e91c7c48e2327c271447bafbfe
tomas-tank3ngine <tomasmartinez1424@gmail.com>
2024-01-26 04:40:56 -0500

    add stylign for mobile

--- src/components/QuizModal/QuizModal.js
+++ src/components/QuizModal/QuizModal.js
@@ -1,5 +1,5 @@
 import "./QuizModal.scss";
-import "../../styles/partials/_buttons.scss";
+// import "../../styles/partials/_buttons.scss";
 import { useEffect, useState } from "react";
 
 function QuizModal() {
@@ -19,71 +19,38 @@ function QuizModal() {
           <section className="close-modal">
             <button className="close-modal__button">X</button>
           </section>
-          <section className="progress-bar">{/* NO IDEA */}</section>
+          <section className="progress-bar">{/* NO IDEA */}
+            PROGRESS BAR          
+          </section>
 
-          <section className="quiz-content">
-            <h2 className="quiz-content__heading">
-              What cruise personality are you?
-            </h2>
-            <h1 className="quiz-content__prompt">
-              Choose your spirit animal
-            </h1>
-            <section className="quiz-options">
-              <button className="quiz-option">
-                <img src="https://via.placeholder.com/40x60/0bf/fff&text=A" alt="" className="quiz-option__image" />
-              </button>
-
-              <button className="quiz-option">
-                <img src="https://via.placeholder.com/40x60/0bf/fff&text=A" alt="" className="quiz-option__image" />
-              </button>
-
-              <button className="quiz-option">
-                <img src="https://via.placeholder.com/40x60/0bf/fff&text=A" alt="" className="quiz-option__image" />
-              </button>
-
-              <button className="quiz-option">
-                <img src="https://via.placeholder.com/40x60/0bf/fff&text=A" alt="" className="quiz-option__image" />
-              </button>
-
-
+            <section className="quiz-text">
+                <h2 className="quiz-text__heading">What cruise personality are you?</h2>
+                <h1 className="quiz-text__prompt">Choose your spirit animal</h1>
             </section>
 
+          <section className="quiz-content">            
+            <section className="quiz-options">
 
-            {/* <form className="quiz-form">
-            <label className="quiz-option">
-              <input className="quiz-option__input" type="radio" name="quiz-options" value="A" checked />
-              <img
-                className="quiz-option__image"
-                src="https://via.placeholder.com/40x60/0bf/fff&text=A"
-                alt="Option 1"
-              />
-            </label>
+                <section className="quiz-options__container">
+                    <button className="quiz-option">
+                        <img src="https://via.placeholder.com/40x60/0bf/fff&text=A" alt="" className="quiz-option__image" />
+                    </button>
 
-            <label>
-              <input type="radio" name="quiz-options" value="B" />
-              <img
-                src="https://via.placeholder.com/40x60/b0f/fff&text=B"
-                alt="Option 2"
-              />
-            </label>
+                    <button className="quiz-option">
+                        <img src="https://via.placeholder.com/40x60/0bf/fff&text=B" alt="" className="quiz-option__image" />
+                    </button>                    
+                </section>
 
-            <label className="quiz-option">
-              <input className="quiz-option__input" type="radio" name="quiz-options" value="C" checked />
-              <img
-                className="quiz-option__image"
-                src="https://via.placeholder.com/40x60/6nf/fff&text=C"
-                alt="Option 3"
-              />
-            </label>
+                <section className="quiz-options__container">
+                    <button className="quiz-option">
+                        <img src="https://via.placeholder.com/40x60/0bf/fff&text=C" alt="" className="quiz-option__image" />
+                    </button>
 
-            <label>
-              <input type="radio" name="quiz-options" value="D" />
-              <img
-                src="https://via.placeholder.com/40x60/00f/fff&text=D"
-                alt="Option 2"
-              />
-            </label>
-          </form> */}
+                    <button className="quiz-option">
+                        <img src="https://via.placeholder.com/40x60/0bf/fff&text=D" alt="" className="quiz-option__image" />
+                    </button>
+                </section>
+            </section>
           </section>
 
           <div className="continue-quiz">
