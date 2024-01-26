75d9c79e4bfb27014f029066d93264775a309d68
Alex Colford <alexcolford@hotmail.com>
2024-01-26 11:34:52 -0500

    Added styling and functionality to modal

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
-
+import QuizModal from "./components/QuizModal/QuizModal";
 
 function App() {
   return (
@@ -9,6 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/survey" element={<QuizModal />} />
         </Routes>
       </BrowserRouter>
       {/* <button>Click me</button> */}
