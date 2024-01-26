b6de21cc55954f9227d9bce563cdd5f35fc11a0c
tomas-tank3ngine <tomasmartinez1424@gmail.com>
2024-01-25 20:24:31 -0500

    add more structure

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route } from 'react-router-dom';
 
@@ -10,7 +9,7 @@ function App() {
 
         <Routes>
           <Route path="/" element="" />
-          
+
 
         </Routes>
       </BrowserRouter>
