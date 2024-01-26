6f1cbcd2751b35ce624437b8e28129bc9d1b34f7
isha <IshaGajendrasingh.Thakur@student.ufv.ca>
2024-01-26 11:09:29 -0800

    added animations to the loading image icons

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,6 +1,5 @@
 import './Loading.scss';
-import blueWhale from "../../assets/images/icons/whales.png";
-
+import whale from "../../assets/images/icons/blueWhaleIcon.png";
 
 function Loading(){
     return(
@@ -10,14 +9,15 @@ function Loading(){
                 <h1 className="loading-container__heading">
                     Loading
                     <div className='loading-container__icons'>
-                        <img src={blueWhale}/>
+                        <img className="first" src={whale} alt="blue-whale-image"/>
+                        <img className="second" src={whale} alt="blue-whale-image"/>
+                        <img className="third" src={whale} alt="blue-whale-image"/>
                     </div>
                 </h1>
 
             </div>
         </main>
     )
-
 }
 
 export default Loading;
