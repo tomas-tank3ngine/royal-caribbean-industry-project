dc09e181ab170ed7de7d49817abb58d73936199a
Veronique Desrochers <veronique.desrochers@hotmail.com>
2024-01-26 14:01:43 -0500

    change all assets for better quality photos

--- src/components/QuizImageHolder/QuizImageHolder.js
+++ src/components/QuizImageHolder/QuizImageHolder.js
@@ -1,19 +1,19 @@
-import dolphin from "../../assets/images/quiz_images/Dolphin.png";
-import seal from "../../assets/images/quiz_images/seal.png";
-import turtle from "../../assets/images/quiz_images/turtle2.png";
-import whale from "../../assets/images/quiz_images/whale.png";
-import pizza from "../../assets/images/quiz_images/pizza.png";
-import salad from "../../assets/images/quiz_images/salad.png";
-import soup from "../../assets/images/quiz_images/soup.png";
-import paella from "../../assets/images/quiz_images/paella.png";
+import dolphin from "../../assets/images/quiz_images/dolphins.jpeg";
+import seal from "../../assets/images/quiz_images/seal.jpeg";
+import turtle from "../../assets/images/quiz_images/turtle.jpeg";
+import whale from "../../assets/images/quiz_images/whale.jpeg";
+import pizza from "../../assets/images/quiz_images/pizza.jpeg";
+import salad from "../../assets/images/quiz_images/salad.jpeg";
+import soup from "../../assets/images/quiz_images/soup.jpeg";
+import paella from "../../assets/images/quiz_images/paella.jpeg";
 import green from "../../assets/images/quiz_images/green.png"
 import red from "../../assets/images/quiz_images/red.png"
 import blue from "../../assets/images/quiz_images/blue.png"
 import purple from "../../assets/images/quiz_images/purple.png"
-import bed from "../../assets/images/quiz_images/bed.png"
-import party from "../../assets/images/quiz_images/party.png"
-import stargaze from "../../assets/images/quiz_images/stargaze.png"
-import midnightDip from "../../assets/images/quiz_images/dip.png"
+import bed from "../../assets/images/quiz_images/bed.jpeg"
+import party from "../../assets/images/quiz_images/party.jpeg"
+import stargaze from "../../assets/images/quiz_images/stargaze.jpeg"
+import midnightDip from "../../assets/images/quiz_images/dip.jpeg"
 
 
 function QuizImages() {
