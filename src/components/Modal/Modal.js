b46d41bd0d7ebfc2d72cc7b812e74fa4380170b4
tank3ngine <117690704+tank3ngine@users.noreply.github.com>
2024-03-10 15:11:06 -0400

    add styling changes and close modal functionality

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -8,39 +8,42 @@ function Modal({ onClosed }) {
     onClosed();
   }
   return (
-    <div className="modal">
-      <section className="modal-content">
-        <section className="modal-content__section modal-content__section--close">
-          <button
-            className="modal-content__section-close-button"
-            onClick={onCancelClicked}
-          >
-            X
-          </button>
+    <div className="fade-background">
+
+      <div className="modal">
+        <section className="modal-content">
+          <section className="modal-content__section modal-content__section--close">
+            <button
+              className="modal-content__section-close-button"
+              onClick={onCancelClicked}
+            >
+              X
+            </button>
+          </section>
+          <section className="modal-content__section modal-content__section--top">
+            <h3 className="modal-content__section-message">
+              Experience your own Royal Adventure
+            </h3>
+          </section>
+          <section className="modal-content__section modal-content__section--middle">
+            <img
+              className="modal-content__section-image"
+              src={modalImage}
+              alt="modal-image"
+            />
+          </section>
+          <section className="modal-content__section modal-content__section--bottom">
+            <button className="modal-content__section-button">
+              <Link className="modal-content__section-link" to={"/survey"}>
+                Take a Survey!
+              </Link>
+            </button>
+            <h3 className="modal-content__section-message">
+              Tailored just for YOU!
+            </h3>
+          </section>
         </section>
-        <section className="modal-content__section modal-content__section--top">
-          <h3 className="modal-content__section-message">
-            Take Part on Your Own Royal Adventure
-          </h3>
-        </section>
-        <section className="modal-content__section modal-content__section--middle">
-          <img
-            className="modal-content__section-image"
-            src={modalImage}
-            alt="modal-image"
-          />
-        </section>
-        <section className="modal-content__section modal-content__section--bottom">
-          <button className="modal-content__section-button">
-            <Link className="modal-content__section-link" to={"/survey"}>
-              Take a Survey!
-            </Link>
-          </button>
-          <h3 className="modal-content__section-message">
-            Adventures Made For YOU!
-          </h3>
-        </section>
-      </section>
+      </div>
     </div>
   );
 }
